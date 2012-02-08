4870029a41342f7b57ac6544d6b36bc5d10d27be
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-08 21:41:41 +0200

    added docs

--- tagger/mgr.py
+++ tagger/mgr.py
@@ -4,15 +4,23 @@ import MySQLdb
 class Mgr:
 	def __init__(self):
 		pass
-	def showconfig(self):
-		tagger.config.show()
-	def testconnect(self):
-		conn=MySQLdb.connect(
+	""" helpers """
+	def connect(self):
+		self.conn=MySQLdb.connect(
 			db=tagger.config.ns_db.p_db,
 			host=tagger.config.ns_db.p_host,
 			port=tagger.config.ns_db.p_port,
 			user=tagger.config.ns_db.p_user,
 			passwd=tagger.config.ns_db.p_password,
 		)
-		conn.close()
+	def disconnect(self):
+		self.conn.close()
+	""" ops that can be launched from the command line """
+	def showconfig(self):
+		tagger.config.show()
+	def testconnect(self):
+		self.connect()
+		self.disconnect()
 		print "connection is ok"
+	def create(self):
+		print "creating..."
