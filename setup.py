da8b06408d80c7c6f8563d0d5a03247b8eeaa880
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-01 18:48:00 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pytags',
-    version='0.0.6',
+    version='0.0.7',
     description='module to help you tag interesting places in your code',
     long_description='module to help you tag interesting places in your code',
     url='https://veltzer.github.io/pytags',
