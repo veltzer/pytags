f178452033b454b5d59fbacac813e351a90bd273
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-09 11:42:39 +0200

    allow only python 3

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools
 
+import sys
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only python version 3 is supported")
+
 setuptools.setup(
     name='pytags',
     version='0.0.7',
