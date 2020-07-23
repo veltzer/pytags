6221f555d8ea2b6badbd3495de94c8c2db9a8fe9
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-24 01:43:21 +0300

    better config file

--- config/general.py
+++ config/general.py
@@ -6,6 +6,4 @@ import subprocess
 general_current_folder = os.path.basename(os.getcwd())
 general_current_year = datetime.datetime.now().year
 general_homedir = os.path.expanduser("~")
-# d.general_hostname=subprocess.check_output(['hostname']).decode().rstrip()
 general_hostname = socket.gethostname()
-general_domain_name = subprocess.check_output(["hostname", "--domain"]).decode().rstrip()
