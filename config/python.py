4b036d9aea17b8b91b91b9e44e94bf03a1180c43
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-20 12:39:52 +0200

    dependencies do not need explanations

--- config/python.py
+++ config/python.py
@@ -10,21 +10,21 @@ setup_requires = [
 ]
 
 run_requires = [
-    'pytconf',  # for command line parsing
-    'pylogconf',  # for logging configuration
+    'pytconf',
+    'pylogconf',
 ]
 
 test_requires = [
-    'pylint',  # to check for lint errors
-    'pytest',  # for testing
-    'pyflakes',  # for testing
+    'pylint',
+    'pytest',
+    'pyflakes',
 ]
 
 dev_requires = [
-    'pyclassifiers',  # for programmatic classifiers
-    'pypitools',  # for upload etc
-    'pydmt',  # for building
-    'Sphinx',  # for the sphinx builder
+    'pyclassifiers',
+    'pypitools',
+    'pydmt',
+    'Sphinx',
 ]
 
 install_requires = list(setup_requires)
@@ -33,5 +33,4 @@ install_requires.extend(run_requires)
 python_requires = ">=3.6"
 
 extras_require={
-#    ':python_version == "2.7"': ['futures'],  # for python2.7 backport of concurrent.futures
 }
